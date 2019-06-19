d70de3e9589979043421d1b62fe38947d417978c
Salman <alk-333@hotmail.com>
2019-06-19 15:50:16 +0300

    update gemfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,7 @@ class HomeController < ApplicationController
         @users = User.where('zip_code' => current_user.zip_code)
         @items = []
         @users.each do |user|
-            if user.zip_code != current_user.zip_code
-                @items.push(User.find(user.id).items)
-            end
+            @items.push(User.find(user.id).items)
         end
     end
 end
