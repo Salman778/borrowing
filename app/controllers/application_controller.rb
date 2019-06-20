3844cd15b7e61bd9b4a7b79a32f241a541ce4020
Salman <alk-333@hotmail.com>
2019-06-20 09:55:01 +0300

    last work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zip_code, :city, :country, :location, :phone_number,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :zip_code, :city, :country, :location, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zip_code, :city, :country, :location, :phone_number])
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,12 @@ class HomeController < ApplicationController
     def index
         @users = User.where('zip_code' => current_user.zip_code)
         @items = []
+        @users_details = []
         @users.each do |user|
-            @items.push(User.find(user.id).items)
+            if user.id != current_user.id
+                @items.push(User.find(user.id).items)
+                @users_details.push(User.find(user.id)) 
+            end
         end
     end
 end
